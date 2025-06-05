001da8c7d0f9176f8cc7b1d4fee1af82527a976a
celekorca <marsel.mano@gmail.com>
2025-06-05 14:05:21 +0200

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Optional: Add basePath if you're deploying to a subdirectory
+  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  // Optional: Enable React Strict Mode
+  reactStrictMode: true,
 };
 
 export default nextConfig;
